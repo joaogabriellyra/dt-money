d4b30f87948f4c5326063a3b896d1ec4d83f65d1
joaogabriellyra <jgabriellyra@hotmail.com>
2024-12-18 10:11:30 -0300

    add a hook to handle with summary component info

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -1,26 +1,11 @@
-import { useContext } from "react"
 import { GreenSummaryContentContainer, SummaryContainer, SummaryContentContainer, SummaryContentValue, SummaryTitleAndIconContainer } from "./styles"
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from '@phosphor-icons/react'
-import { TransactionsContext } from "../../contexts/transactions-context"
 import { priceFormatter } from "../../utils/formatter"
+import { useSummary } from "../../hooks/useSummary"
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext)
-  const summary = transactions.reduce((acc, transaction) => {
+  const summary = useSummary()
 
-    if (transaction.type === 'income') {
-      acc.income += transaction.price
-      acc.total += transaction.price
-    } else {
-      acc.outcome -= transaction.price
-      acc.total -= transaction.price
-    }
-    return acc
-  }, {
-    outcome: 0,
-    income: 0,
-    total: 0
-  })
   return (
     <SummaryContainer>
       <SummaryContentContainer>
